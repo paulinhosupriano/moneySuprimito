12285d8e6532ee71abfa71dc8a9b2a2fd2f5ebfb
MITRA PC - JP <mitrafortalezainformatica@gmail.com>
2025-09-04 08:04:13 -0300

    project tabs

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function LayoutTabs() {
+  return (
+    <View>
+      <Text>Tabs</Text>
+    </View>
+  );
+}
---
+++ app/+not-found.jsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function NotFoundScreen() {
+  return (
+    <View>
+      <Text>Página não encontrada</Text>
+    </View>
+  );
+}
